ea2d3d176cc16bc05a1431a54bfacb384db5a10a
baryo <or.bar.yosef@sap.com>
2021-11-18 02:18:09 +0200

    replacing app with submodule

--- exercises/Angular-ShoppingCart/src/environments/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export const FireBaseConfig = {
-  apiKey: "AIzaSyA6yR7_Dlr1GOcxxAVLaokQMuq5--FMyLM",
-  authDomain: "angular-shoping-cart-d1042.firebaseapp.com",
-  projectId: "angular-shoping-cart-d1042",
-  storageBucket: "angular-shoping-cart-d1042.appspot.com",
-  messagingSenderId: "751757086055",
-  appId: "1:751757086055:web:e3711e233ffd48436d33c2",
-  measurementId: "G-G5CC7S8N1N",
-};
